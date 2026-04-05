d3f5ae4a9b7c7c3d7165a10ed4bd085be1309abe
Hervey67 <herv2007@gmail.com>
2026-04-04 20:02:45 -0700

    Changes to main_write.cpp

--- main_write.cpp
+++ main_write.cpp
@@ -8,18 +8,26 @@ int main()
 {
   ofstream file;
   string sname;
-  int score1, score2, N;
-  cin >> N;
+  int N;
+  double score1, score2; 
+
   file.open("Students.txt");
+
   if (!file.is_open()){
     cerr << "Error opening file!" << endl;
     return 1;
   }
+  cout << "Enter the total number of students";
+
+  cin >> N; 
   file << N << endl;
+
   for (int i=0; i<N; i++){
+    cout << "Enter student name and two scores; "; 
     cin >> sname >> score1 >> score2;
     file << sname << " " << score1 << " " << score2 << endl;
 
   }
+    file.close();
     return 0;
 }
